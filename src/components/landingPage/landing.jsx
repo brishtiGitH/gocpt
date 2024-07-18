74c11461f5f6adf587b38c77a729318091cf011a
Brishti Das <das.brishti342@gmail.com>
2024-07-18 22:34:14 +0530

    modify hero

--- src/components/landingPage/landing.jsx
+++ src/components/landingPage/landing.jsx
@@ -6,17 +6,19 @@ const LandingPage = () => {
     <div className="landing-page-container w-full min-h-full flex items-center">
       <div className="content p-10 flex flex-col 2xl:flex-row w-full items-center justify-center gap-10">
         <div className="left-content w-full 2xl:w-1/2 h-full flex flex-col justify-center gap-10 p-10 m-5 items-start">
-          <h1 className="text-5xl">
+          <h1 className=" text-2xl md:text-4xl lg:text-5xl">
             Welcome to <span className="logo1">Go</span>
             <span className="logo2">CPT</span> BETA Your Path to Effortless CPT
             Coding!
           </h1>
-          <p className="text-3xl">
+          <p className="text-xl md:text-2xl lg:text-3xl">
             We’re excited to introduce you to the BETA version of GoCPT Your
             free pass to a simplified and efficient CPT coding experience
             designed exclusively for mental health professionals like you.
           </p>
-          <button className="btn text-3xl lg:w-[375px]">Start Testing</button>
+          <button className="btn text-xl md:text-2xl lg:text-3xl lg:w-[375px]">
+            Start Testing
+          </button>
         </div>
         <div className="rightw-full 2xl:w-1/2"></div>
       </div>
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,7 +5,7 @@ import { Link, useNavigate } from "react-router-dom";
 const Navbar = ({ scrollToFeaturePage, scrollToAbout, scrollToContact }) => {
   const navigate = useNavigate();
   return (
-    <div className="navbar z-50 top-0 left-0 right-0 fixed w-full flex gap-0 lg:gap-5 justify-evenly lg:justify-between items-center py-3 bg-opacity-5 backdrop-filter backdrop-blur-2xl">
+    <div className="navbar z-50 top-0 left-0 right-0 fixed  flex gap-0 lg:gap-5 justify-evenly lg:justify-between items-center py-3 bg-opacity-5 backdrop-filter backdrop-blur-2xl">
       <h1 className="text-4xl md:text-4xl lg:text-5xl xl:text-6xl drop-shadow-2xl font-extrabold">
         <span className="logo1">Go</span>
         <span className="logo2">CPT</span>
